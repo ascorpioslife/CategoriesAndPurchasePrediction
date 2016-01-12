deffb03e43f40c88d7e6c97553c6b495bdbb967b
Nik <nikunjj46@gmail.com>
2016-01-13 02:08:24 +0530

    built dictionary

--- Categories.R
+++ Categories.R
@@ -6,4 +6,18 @@ categories <- rm_between(catdata$cat,extract = TRUE,"\"", "\"")
 categories <- unique(unlist(categories))
 cat.list <- as.list(categories)
 
+for(i in 1:length(categories))
+{
+    word <- categories[i]
+    for(j in 1:nrow(catdata))
+    { 
+      if(length(grep(word,catdata[j,4])))
+      {
+        cat.list[word]=paste(cat.list[word],catdata[j,2])
+      }
+      
+    }
+}
+
+
 
